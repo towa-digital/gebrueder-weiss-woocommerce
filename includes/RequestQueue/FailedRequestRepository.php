ab5db47736a7e45fd4bb361df41ffbbda37c17d5
philipp-hemmer <philipp.hemmer@towa.at>
2021-07-30 15:36:15 +0000

    feat: added Repository & Worker drafts

---
+++ includes/RequestQueue/FailedRequestRepository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Failed Request Repository
+ *
+ * Holds FailedRequests.
+ *
+ * @package GbWeissOptions
+ */
+
+namespace GbWeiss\includes;
+
+
+defined('ABSPATH') || exit;
+
+/**
+ * FailedRequest Class
+ */
+class FailedRequestRepository
+{
+}
