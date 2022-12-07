a4f2e2713c2a7984af22f483e043404fc5f94ff0
adam.oerszigety <adam.oerszigety@towa.at>
2022-12-07 13:26:28 +0100

    fix: type definitions in php docs

--- includes/SettingsRepository.php
+++ includes/SettingsRepository.php
@@ -132,7 +132,7 @@ class SettingsRepository
     /**
      * Reads the custom field name for the tracking link from the plugin settings.
      *
-     * @return integer|null
+     * @return string
      */
     public function getTrackingLinkFieldName(): string
     {
@@ -142,7 +142,7 @@ class SettingsRepository
     /**
      * Reads the custom field name for the carrier information from the plugin settings.
      *
-     * @return integer|null
+     * @return string
      */
     public function getCarrierInformationFieldName(): string
     {
